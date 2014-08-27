a599c7c41df05c69f29b9ff01ba41d9b351601cd
info@ewallah.net <info@ewallah.net>
2014-08-27 16:31:36 +0200

    version update

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014082704;
+$plugin->version = 2014082705;
 $plugin->requires = 2014050800;
 $plugin->release = '1.0';
 $plugin->maturity = MATURITY_BETA;
