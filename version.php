7289784b308e354a4d0826399ad9e43f7a028b04
eWallah <info@eWallah.net>
2019-05-13 09:27:38 +0200

    requires downgrading

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2019051200;
-$plugin->requires  = 2018051700;
+$plugin->requires  = 2016120500;
 $plugin->component = 'availability_language';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.5+';
+$plugin->release   = '3.2+';
