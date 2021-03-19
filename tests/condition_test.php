99573b8bbbbac622db41e6cceafd9650ee12b577
eWallah <info@eWallah.net>
2021-03-19 19:45:34 +0100

    cron

--- tests/condition_test.php
+++ tests/condition_test.php
@@ -240,7 +240,7 @@ class availability_language_condition_testcase extends advanced_testcase {
      * @coversDefaultClass availability_language\condition
      */
     public function test_backend() {
-        global $DB;
+        global $CFG, $DB;
         $this->resetAfterTest();
         $this->setAdminUser();
         set_config('enableavailability', true);
@@ -260,9 +260,19 @@ class availability_language_condition_testcase extends advanced_testcase {
         $mpage = new moodle_page();
         $mpage->set_url('/course/index.php', ['id' => $course->id]);
         $mpage->set_context($context);
+        $format = course_get_format($course);
         $renderer = $mpage->get_renderer('format_topics');
-        ob_start();
-        echo $renderer->print_multiple_section_page($course, null, null, null, null);
+        $branch = null;
+        require("$CFG->dirroot/version.php");
+        if ($branch > 310) {
+            $outputclass = $format->get_output_classname('course_format');
+            $output = new $outputclass($format);
+            ob_start();
+            echo $renderer->render($output);
+        } else  {
+            ob_start();
+            echo $renderer->print_multiple_section_page($course, null, null, null, null);
+        }
         $out = ob_get_clean();
         $this->assertStringContainsString('Not available unless: The student\'s language is English ‎(en)', $out);
         // MDL-68333 hack when nl language is not installed.
